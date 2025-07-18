03bdf864e6475f2a968b08cd104e6270918e7165
Arthur Atul <arthur.atul@rubicotech.in>
2025-07-18 16:16:07 +0530

    step1: chat without memory

--- backend/src/agents/agents.service.ts
+++ backend/src/agents/agents.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAgentDto } from './dto/create-agent.dto';
 import { UpdateAgentDto } from './dto/update-agent.dto';
+import OpenAI from 'openai';
 
 @Injectable()
 export class AgentsService {
-  create(createAgentDto: CreateAgentDto) {
-    return 'This action adds a new agent';
-  }
+  async getOpenAIResponse(message: string): Promise<string> {
 
+    console.log(process.env.OPENAI_API_KEY);
+   const client = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+   });
+
+   const response = await client.responses.create({
+    model: 'gpt-4o-mini',
+    input: message,
+    max_output_tokens: 100,
+   })
+
+   return response.output_text;
+  }
 }
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AgentsModule } from './agents/agents.module';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [AgentsModule],
+  imports: [AgentsModule, ConfigModule.forRoot({
+    isGlobal: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
