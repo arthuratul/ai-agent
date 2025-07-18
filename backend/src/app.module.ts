3119fd35db1c8b21ccce3a75e510c9627c090248
Arthur Atul <arthur.atul@rubicotech.in>
2025-07-18 16:44:34 +0530

    add mongoose module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AgentsModule } from './agents/agents.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [AgentsModule, ConfigModule.forRoot({
     isGlobal: true,
+  }),MongooseModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: async (configService: ConfigService) => ({
+      uri: configService.get<string>('MONGO_URI'),
+    }),
+    inject: [ConfigService],
   })],
   controllers: [AppController],
   providers: [AppService],
